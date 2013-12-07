8510c44e7ccbf99401a1f6cd74a0ce765bba8fb6
fabianvf <fabian@fabianism.us>
2013-12-07 15:42:23 -0500

    Added equality expressions

--- fsv5d/src/pp5/ast_expr.cc
+++ fsv5d/src/pp5/ast_expr.cc
@@ -173,6 +173,19 @@ void RelationalExpr::Emit(CodeGenerator *cg){
 }
 
 void EqualityExpr::Emit(CodeGenerator *cg){
+// Equality ops available in tac file : "=="
+    std::string opName = std::string(op->str());
+    left->Emit(cg);
+    right->Emit(cg);
+    // uses "==", only defined equality operator
+    if(opName == "=="){
+	offsetLoc = cg->GenBinaryOp("==", left->offsetLoc, right->offsetLoc);
+    }
+    // 1 != 2 is equivalent to (1 == 2) == false (0)
+    else if(opName == "!="){
+	Location* eq = cg->GenBinaryOp("==", left->offsetLoc, right->offsetLoc);
+	offsetLoc = cg->GenBinaryOp("==", eq, cg->GenLoadConstant(0));	
+    }
 
 }
 
